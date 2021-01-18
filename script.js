1ec50666a86f1ba9b9ad726f409b8b12f3935803
Luiz Marcelo Torre <luizmptorre@gmail.com>
2021-01-17 22:39:18 -0300

    MVP 4 finalizado

--- script.js
+++ script.js
@@ -3,18 +3,24 @@ function enviar() {
 var comentario_tudo = document.createElement('div');
 var topo_comentario = document.createElement('div');
 var nome_comentario = document.createElement('text');
+var edit = document.createElement('div');
+
+
 var botoes = document.createElement('div');
 
 var apagar_comentario = document.createElement('button');
 var txt_comentario = document.createElement('div');
 var editar_comentario = document.createElement('button');
 
+
+// dando Class às tags
 comentario_tudo.className = 'comentario_tudo';
 topo_comentario.className = 'topo_comentario';
-
 nome_comentario.className = 'nome_comentario';
-
 txt_comentario.className = 'txt_comentario';
+edit.className = 'edit';
+
+
 
 //Dando os ids dos botões
 apagar_comentario.id = 'apagar_comentario';
@@ -40,6 +46,8 @@ topo_comentario.appendChild(botoes)
 
 comentario_tudo.appendChild(topo_comentario)
 comentario_tudo.appendChild(txt_comentario)
+comentario_tudo.appendChild(edit)
+
 
 
 document.getElementById('com').appendChild(comentario_tudo);
@@ -52,9 +60,7 @@ function apagar(){
     list.removeChild(list.firstChild);
 }
 
-
 }
-
 
 function removeLinha(removedor){
     var removed = removedor.closest('div').parentNode.parentNode
@@ -63,19 +69,20 @@ function removeLinha(removedor){
 
 
 function editar(aux_editar){
-    var aux_edita = aux_editar.closest('div').previousSibling
-    var aux_edita2 = aux_editar.closest('div').parentNode.parentNode.lastChild
+    var aux_edita = aux_editar.closest('div').parentNode.parentNode.lastChild
 
-    aux_edita.innerHTML = '<label>Editar Nome:</label><input type="text" id="nome_editar" required="true">'
-    aux_edita2.innerHTML = '<label>Editar Nome:</label><input type="text" id="coment_editar" required="required"><div class = "botão_editar_confirmar"><button class="confirma_edita" onClick = "altera(this)"></div>'
+    aux_edita.innerHTML = '<div class="Mega_edit"><div class="edita_nome_design"><label>Editar Nome:</label><input type="text" id="nome_editar"></div><br><div class = "edita_comentario_design"><br><label>Editar Comentario:</label><textarea id="coment_editar"></textarea></div><div class = "botão_editar_confirmar"><button class="confirma_edita" onClick = "altera(this)"></div>'
 }
 
- function altera(aux_altera){
-    var alterar = aux_altera.closest('div').parentNode.parentNode.firstChild.firstChild
-    var alterar2 = aux_altera.closest('div').parentNode
 
-    alterar.innerHTML = document.querySelector('#nome_editar').value;
+function altera(aux_altera){
+    var alterar = aux_altera.closest('div').parentNode.parentNode.parentNode.firstChild.firstChild
+    var alterar2 = aux_altera.closest('div').parentNode.parentNode.parentNode.firstChild.nextSibling
+    var alterar3 = aux_altera.closest('div').parentNode
+
+    alterar.innerText = document.querySelector('#nome_editar').value;
     alterar2.innerHTML = document.querySelector('#coment_editar').value;
+    alterar3.outerHTML = '';
 
     
  }
